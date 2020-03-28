b196c712e4e4593039fa280a111e600a3b19eea5
theVTOQ <vonfunkhouserprod@gmail.com>
2020-03-28 10:31:09 -0700

    Update Github credentials.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
-    binding.pry
     # After entering a name and email value in the /auth/developer
     # path and submitting the form, you will see a pretty-print of
     # the authentication data object that comes from the "developer"
