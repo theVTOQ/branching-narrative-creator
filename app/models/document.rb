2343f40c6455e5e649452ae5687091401fb1455e
theVTOQ <vonfunkhouserprod@gmail.com>
2020-03-29 15:23:28 -0700

    Add a spec file for Document model.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,9 @@
 class Document < ApplicationRecord
+    include ActiveModel::Validations
+    validates :narrative_id, presence: true
+    validates :title, presence: true, length: { minimum: 2, maximum: 40}
+    validates_with TitleValidator
+
     has_and_belongs_to_many :parent_documents, class_name: Document, join_table: "branches", foreign_key: "child_document_id", association_foreign_key: "parent_document_id"
     has_and_belongs_to_many :child_documents, class_name: Document, join_table: "branches", foreign_key: "parent_document_id", association_foreign_key: "child_document_id"
     has_one :root_narrative, class_name: Narrative, optional: true #one document per narrative will act as a root document, and therefore will store the id of the narrative
@@ -18,7 +23,8 @@ class Document < ApplicationRecord
     end
 
     def self.eligible_parent_documents(document)
-        self.all.where(narrative_id: document.narrative.id).where.not(child_documents_ids: document.child_documents_ids)
+        self.all.where(narrative_id: document.narrative.id).where.not(child_document_ids: document.child_document_ids)
+        #direct child documents of this document are not eligible to also be direct parents of this document
     end
 
     
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe Document, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Document, :type => :model do
+  let(:user) {
+    User.create(
+      :name => "Wilfred",
+      :password => "will will",
+      :email => "will@will.com"
+    )
+  }
+
+  let(:narrative) {
+    Narrative.create(
+      :title => "Equinox",
+      :author_id => user.id
+    )
+  }
+
+  let(:document) {
+    Document.create(
+      :title => "Perform the ritual",
+      :passage => "The ritual is performed. Now, project solemnity or triumph?",
+      :narrative_id => narrative.id
+    )
+  }
+
+  it "is valid with a title, passage, and narrative_id" do
+    expect(document).to be_valid
+  end
+
+  it "is valid even if given a title from a document in an different existing narrative" do
+    narrative_2 = Narrative.create(title: "Solstice", author_id: user.id)
+    narrative_2.root_document = Document.create(title: "Perform the ritual", narrative_id: narrative_2.id)
+    expect(narrative_2.root_document).to be_valid
+  end
+
+  it "is invalid if given the same title as an existing document in the same narrative" do
+    identical_document = Document.create(title: "Perform the ritual", narrative_id: narrative.id)
+    expect(identical_document).to be_invalid
+  end
+
+  it "has many parent and child branches" do
+    triumph_choice = Document.create(title: "Project triumph")
+    triumph_branch = Branch.create(parent_document_id: document.id, child_document_id: triumph_choice.id)
+    solemnity_choice = Document.create(title: "Project solemnity")
+    solemnity_branch = Branch.create(parent_document_id: document.id, child_document_id: solemnity_choice.id)
+    document.child_branches << triumph_branch
+    document.child_branches << solemnity_branch
+
+    expect(document.child_documents).to eq([triumph_choice, solemnity_choice])
+    expect(triumph_branch.parent_documents).to eq([document])
+  end
 end
